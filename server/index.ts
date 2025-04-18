2da495da5e3d70e4c73f7f11ebb41038b3923039
SevbanBlc <142054445+SevbanBlc@users.noreply.github.com>
2025-04-18 14:14:15 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://vrintership.onrender.com/",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
